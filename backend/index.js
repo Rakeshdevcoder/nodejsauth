186cd64e67ff3e0ee13b8eb23662cdaabc6b7ef2
Rakeshdevcoder <rakesh123dmx@gmail.com>
2025-03-05 13:37:20 +0530

    auth code complete

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config({ path: ".env.local" });
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
